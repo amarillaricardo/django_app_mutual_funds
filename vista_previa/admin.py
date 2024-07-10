788994e1fee8ce35de6cd6b41fe943b6ff152da3
ramarilla <amarilla_ricardo@hotmail.com>
2024-07-10 13:30:20 -0300

    ORM

--- vista_previa/admin.py
+++ vista_previa/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from vista_previa.models import Producto
+
+admin.site.register(Producto)
--- vista_previa/models.py
+++ vista_previa/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    producto = models.CharField(max_length=200)
+    fecha_publicacion = models.DateTimeField('Fecha de publicación')
